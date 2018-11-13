885d1eb1e077ab932d715eff9dcfbf2021fed122
Mekkala <mbourapa1@buffs.wtamu.edu>
2018-11-12 23:42:48 -0600

    Push cal library

---
+++ MyMath.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class MyMath
+    {
+       public static decimal Add(decimal left, decimal right)
+        {
+            return left + right;
+        }
+
+        public static decimal Subtract(decimal left, decimal right)
+        {
+            return left + right;
+
+        }
+
+        public static decimal Multiply(decimal left, decimal right)
+        {
+            return left + right;
+
+        }
+
+        public static decimal Divide(decimal left, decimal right)
+        {
+            return left + right;
+
+        }
+
+        public static double Power(double x , double y)
+        {
+            return Math.Pow(x, y);
+
+        }
+
+        public static decimal Celling(decimal number)
+        {
+            return Math.Ceiling(number);
+
+        }
+
+        public static decimal Floor(decimal number)
+        {
+            return Math.Floor(number);
+
+        }
+    }
+}
