39a8c1dfce81270b8edb41778e4faa9a43b455ba
Mekkala <mbourapa1@buffs.wtamu.edu>
2018-11-25 11:25:21 -0600

    Submit assignment 7

---
+++ Models/ConverterViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment7.Models
+{
+    public class ConverterViewModel
+    {
+        public decimal ValueToConvert {get; set; }
+        public string ConversionType {get; set; }
+        public decimal ConvertedValue {get; set; }
+    }
+}
